548e0393af1f7e91687583b3f91e68fe8d2fea04
ZHONG007 <zhongwang07@yahoo.com>
2023-03-10 18:39:09 +0000

    Update main.go

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
         "fmt"
         "context"
         "github.com/aws/aws-lambda-go/lambda"
+	"log"
 )
 
 type MyEvent struct {
         Message string `json:"message"`
 }
 
+
+
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	log.Printf("Received message: %s", name.Message)
         return fmt.Sprintf("Hello %s!", name.Message ), nil
 }
 
