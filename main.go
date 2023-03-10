f5e3e4f2f6402dca39b5c1f07d05cff4942cb90b
ZHONG007 <zhongwang07@yahoo.com>
2023-03-10 16:34:04 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,23 +1,16 @@
 package main
+
 import (
-        "context"
-        "fmt"
-        "net/http"
-    
-        "github.com/aws/aws-lambda-go/events"
-        "github.com/aws/aws-lambda-go/lambda"
-    )
- 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-        fmt.Println("Received request:", request)
-    
-        return events.APIGatewayProxyResponse{
-            StatusCode: http.StatusOK,
-            Body:       "hello from golang in aws lambda",
-        }, nil
-    }
- 
-    func main() {
-        fmt.Println("Received request start")
-        lambda.Start(handler)
-    }
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, payload []byte) {
+	fmt.Printf("Received message: %s\n", string(payload))
+}
+
+func main() {
+	lambda.Start(handler)
+}
