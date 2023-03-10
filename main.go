9fe0204c700a0863350d14469f5d6624762d4be0
ZHONG007 <zhongwang07@yahoo.com>
2023-03-10 14:23:58 +0000

    http request and return

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
- 
 import (
+        "context"
         "fmt"
+        "net/http"
+    
+        "github.com/aws/aws-lambda-go/events"
         "github.com/aws/aws-lambda-go/lambda"
-)
-
-type MyEvent struct {
-        Name string `json:"What is your name?"`
-        Age int     `json:"How old are you?"`
-}
+    )
  
-type MyResponse struct {
-        Message string `json:"Answer:"`
-}
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+        fmt.Println("Received request:", request)
+    
+        return events.APIGatewayProxyResponse{
+            StatusCode: http.StatusOK,
+            Body:       "hello from golang in aws lambda",
+        }, nil
+    }
  
-func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-        return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
-}
- 
-func main() {
-        lambda.Start(HandleLambdaEvent)
-}
+    func main() {
+        fmt.Println("Received request start")
+        lambda.Start(handler)
+    }
