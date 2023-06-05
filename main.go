d5ed037a2c6e534f5b4b3dd730b2d3eb9265e014
ZHONG007 <zhongwang07@yahoo.com>
2023-06-05 16:08:31 +0100

    Update main.go
    http request

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
         "context"
         "github.com/aws/aws-lambda-go/lambda"
 	"log"
+	"io/ioutil"
+	"net/http"
 )
 
 type MyEvent struct {
@@ -14,12 +16,28 @@ type MyEvent struct {
 
 
 
+
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
 	log.Printf("Received message: %s", name.Message)
+        response, err := http.Get("https://8080-cs-476722282134-default.cs-europe-west1-iuzs.cloudshell.dev/sensors")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(string(body))
         return fmt.Sprintf("Hello %s!", name.Message ), nil
 }
 
 func main() {
+
+	
+	
 	fmt.Sprintf("Hello!" )
         lambda.Start(HandleRequest)
 }
