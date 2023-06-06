c64dc7aba3175944d331f387bd4d064f5dac473b
ZHONG007 <zhongwang07@yahoo.com>
2023-06-06 14:36:12 +0100

    Update main.go

--- main.go
+++ main.go
@@ -9,14 +9,22 @@ import (
 )
 
 type MyEvent struct {
-        Data string `json:"data"`
+        Data []string `json:"data"`
 }
 
 
 
 
+
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	log.Printf("Received message: %s", name.Data)
+	for _, element := range name.Data {
+                str, ok := element.(string)
+                if ok {
+                        log.Printf("Data: %s", str)
+                } else {
+                        log.Printf("Received non-string data")
+                }
+        }
         return fmt.Sprintf("Hello %s!", name.Data ), nil
 }
 
